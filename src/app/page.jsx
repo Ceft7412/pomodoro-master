21032feb709ce7c40b0d9b871a74eb7839e0b6fa
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-07 21:06:20 +0800

    added: useContext

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,31 @@
+"use client";
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import React from "react";
 import { BsArrowRepeat } from "react-icons/bs";
 import { GrFormNextLink } from "react-icons/gr";
 
+import { RootContext, RootProvider } from "@/context/RootContext";
+
 export default function Home() {
+  const { timer } = React.useContext(RootContext);
+
   return (
     <>
       <div className="skeleton">
         <div class="background">
           <Menu />
           <div class="content">
-            <p className="content__timer">15:25</p>
-            <div className="content__flex">
-              <div className="content__icon">
-                <BsArrowRepeat fontSize={30} />
-              </div>
-              <span>Start</span>
-              <div className="content__icon">
-                <GrFormNextLink fontSize={40} />
+            <div className="content__flex-col">
+              <p className="content__timer">{timer}</p>
+              <div className="content__flex">
+                <div className="content__icon">
+                  <BsArrowRepeat fontSize={30} />
+                </div>
+                <span className="content__start">Start</span>
+                <div className="content__icon">
+                  <GrFormNextLink fontSize={40} />
+                </div>
               </div>
             </div>
           </div>
