e844f0494604aaee7fdc41e5a43b28a857487755
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-07 19:52:48 +0800

    added: divs for the background

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="container">
-        <p>I am Cedrick Caceres</p>
+      <div className="skeleton">
+        <div class="background">
+          <div className="content">
+            <p>15:25</p>
+          </div>
+        </div>
       </div>
     </>
   );
