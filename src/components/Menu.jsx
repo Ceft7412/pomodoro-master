f7ad58fdc3575714760f484d55c385f03f313c77
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 06:39:55 +0800

    added: events for active and set active

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-
+import { RootContext } from "@/context/RootContext";
 export default function Menu() {
+  const { active, setActive } = React.useContext(RootContext);
   return (
     <>
       <div className="menu">
         <div className="menu__flex">
-          <div className="menu__item">
+          <div
+            className={`menu__item ${active === "pomodoro" ? "active" : ""}`}
+            onClick={setActive("pomodoro")}
+          >
             <h1>Pomodoro</h1>
           </div>
-          <div className="menu__item">
+          <div
+            className={`menu__item ${active === "shortbreak" ? "active" : ""}`}
+            onClick={setActive("shortbreak")}
+          >
             <h1>Short Break</h1>
           </div>
-          <div className="menu__item">
+          <div
+            className={`menu__item ${active === "longbreak" ? "active" : ""}`}
+            onClick={setActive("longbreak")}
+          >
             <h1>Long Break</h1>
           </div>
         </div>
