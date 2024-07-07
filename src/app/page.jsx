7329b1158b11966057bcac3cb46c8369ef841568
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 07:35:35 +0800

    added: handle next function to calculate the index of the next state

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,8 @@ import { GrFormNextLink } from "react-icons/gr";
 import { RootContext } from "@/context/RootContext";
 
 export default function Home() {
-  const { timer, startTimer, pauseTimer, clearTimer } = React.useContext(RootContext);
+  const { timer, startTimer, pauseTimer, clearTimer, active, setActive } =
+    React.useContext(RootContext);
 
   const [pause, setPause] = React.useState(false);
 
@@ -24,6 +25,30 @@ export default function Home() {
   const handleClear = () => {
     clearTimer();
     setPause(false);
+  };
+
+  const handleNext = () => {
+    setActive((prevActive) => {
+      // Create a new object with all states set to false
+      const newStateObject = Object.keys(prevActive).reduce((acc, key) => {
+        acc[key] = false;
+        return acc;
+      }, {});
+
+      // Get the keys of the prevActive object
+      const keys = Object.keys(prevActive);
+
+      // Find the index of the currently active state
+      const currentIndex = keys.findIndex((key) => prevActive[key]);
+
+      // Calculate the index of the next state
+      const nextIndex = (currentIndex + 1) % keys.length;
+
+      // Set the next state to true
+      newStateObject[keys[nextIndex]] = true;
+
+      return newStateObject;
+    });
   };
   return (
     <>
@@ -42,7 +67,7 @@ export default function Home() {
                 <div className="content__start content__events" onClick={handleTimer}>
                   <span>{pause ? "Pause" : "Start"}</span>
                 </div>
-                <div className="content__icon content__events">
+                <div className="content__icon content__events" onClick={handleNext}>
                   <GrFormNextLink fontSize={40} />
                 </div>
               </div>
