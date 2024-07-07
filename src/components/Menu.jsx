546f7ae4686460c7a83c523fc8eaab594ddb5c41
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 06:48:23 +0800

    added: onClicks for each menu item

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,19 +8,19 @@ export default function Menu() {
         <div className="menu__flex">
           <div
             className={`menu__item ${active === "pomodoro" ? "active" : ""}`}
-            onClick={setActive("pomodoro")}
+            onClick={() => setActive("pomodoro")}
           >
             <h1>Pomodoro</h1>
           </div>
           <div
             className={`menu__item ${active === "shortbreak" ? "active" : ""}`}
-            onClick={setActive("shortbreak")}
+            onClick={() => setActive("shortbreak")}
           >
             <h1>Short Break</h1>
           </div>
           <div
             className={`menu__item ${active === "longbreak" ? "active" : ""}`}
-            onClick={setActive("longbreak")}
+            onClick={() => setActive("longbreak")}
           >
             <h1>Long Break</h1>
           </div>
