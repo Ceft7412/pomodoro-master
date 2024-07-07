794fa222be07007bac42d473c3885eaf0a2353d4
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 05:01:36 +0800

    created: a start timer arrow function

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -5,10 +5,17 @@ const RootContext = React.createContext();
 
 const RootProvider = ({ children }) => {
   const [timer, setTimer] = React.useState(1500);
+  let interval = null;
 
+  const startTimer = () => {
+    if (interval !== null) {
+      clearInterval(interval);
+    }
 
-  
-  const startTimer = () => {};
+    interval = setInterval(() => {
+      setTimer((prevTime) => (prevTime = 1));
+    }, 1000);
+  };
   return (
     <RootContext.Provider value={{ timer, setTimer: startTimer }}>
       {children}
