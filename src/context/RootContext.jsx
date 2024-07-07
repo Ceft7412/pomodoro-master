f643b4ed7643e049b0a8a8b5f985835fbc6c625c
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 05:03:06 +0800

    created: a pause timer function

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -16,8 +16,18 @@ const RootProvider = ({ children }) => {
       setTimer((prevTime) => (prevTime = 1));
     }, 1000);
   };
+
+  const pauseTimer = () => {
+    if (interval !== null) {
+      clearInterval(interval);
+      interval = null;
+    }
+  };
+  const displayTimer = `${Math.floor(timer / 60)}:${timer % 60 < 10 ? "0" : ""}${
+    timer % 60
+  }`;
   return (
-    <RootContext.Provider value={{ timer, setTimer: startTimer }}>
+    <RootContext.Provider value={{ timer: displayTimer, startTimer, pauseTimer }}>
       {children}
     </RootContext.Provider>
   );
