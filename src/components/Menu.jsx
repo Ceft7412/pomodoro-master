58637f8d2630b369137fc5608e6197809c12ef6d
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-07 21:06:26 +0800

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,19 @@ import React from "react";
 export default function Menu() {
   return (
     <>
-      <div className="menu">What</div>
+      <div className="menu">
+        <div className="menu__flex">
+          <div className="menu__item">
+            <h1>Pomodoro</h1>
+          </div>
+          <div className="menu__item">
+            <h1>Short Break</h1>
+          </div>
+          <div className="menu__item">
+            <h1>Long Break</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
