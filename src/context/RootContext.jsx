9f4bbfc0d539c911551f57e172cfab75ada80460
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 06:22:25 +0800

    updated: show a leading 0 when it's a single digit

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -5,11 +5,10 @@ const RootContext = React.createContext();
 
 const RootProvider = ({ children }) => {
   const [timer, setTimer] = React.useState(1500);
-  console.log(timer);
+
   let interval = React.useRef(null);
 
   const startTimer = () => {
-    console.log("start");
     if (interval.current !== null) {
       clearInterval(interval.current);
     }
@@ -20,15 +19,14 @@ const RootProvider = ({ children }) => {
   };
 
   const pauseTimer = () => {
-    console.log("pause");
     if (interval.current !== null) {
       clearInterval(interval.current);
       interval.current = null;
     }
   };
-  const displayTimer = `${Math.floor(timer / 60)}:${timer % 60 < 10 ? "0" : ""}${
-    timer % 60
-  }`;
+  const displayTimer = `${Math.floor(timer / 60) < 10 ? "0" : ""}${Math.floor(
+    timer / 60
+  )}:${timer % 60 < 10 ? "0" : ""}${timer % 60}`;
   return (
     <RootContext.Provider value={{ timer: displayTimer, startTimer, pauseTimer }}>
       {children}
