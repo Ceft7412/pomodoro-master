c0ac30a1a272b0ee33ca1596f52da4e4eed7a3ad
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-07 19:59:20 +0800

    added: menu component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Menu from "@/components/Menu";
 
 export default function Home() {
   return (
     <>
       <div className="skeleton">
         <div class="background">
+          <Menu />
           <div className="content">
             <p>15:25</p>
           </div>
