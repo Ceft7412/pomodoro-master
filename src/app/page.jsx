94883ea9aafcd267c143d0a5c773f401edd0898a
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-07 20:11:03 +0800

    added: classes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import { BsArrowRepeat } from "react-icons/bs";
+import { GrFormNextLink } from "react-icons/gr";
 
 export default function Home() {
   return (
@@ -7,8 +9,17 @@ export default function Home() {
       <div className="skeleton">
         <div class="background">
           <Menu />
-          <div className="content">
-            <p>15:25</p>
+          <div class="content">
+            <p className="content__timer">15:25</p>
+            <div className="content__flex">
+              <div className="content__icon">
+                <BsArrowRepeat fontSize={30} />
+              </div>
+              <span>Start</span>
+              <div className="content__icon">
+                <GrFormNextLink fontSize={40} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
