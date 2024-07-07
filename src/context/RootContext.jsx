6b36f3ba3aadfbae8b6c6358a8c0b008459dfe20
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 06:40:25 +0800

    added: active state variable

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -6,7 +6,7 @@ const RootContext = React.createContext();
 const RootProvider = ({ children }) => {
   const choseTimer = 1500;
   const [timer, setTimer] = React.useState(choseTimer);
-
+  const [active, setActive] = React.useState("pomodoro");
   let interval = React.useRef(null);
 
   const startTimer = () => {
@@ -29,13 +29,22 @@ const RootProvider = ({ children }) => {
   const clearTimer = () => {
     clearInterval(interval.current);
     interval.current = null;
-    setTimer(1500);
+    setTimer(choseTimer);
   };
   const displayTimer = `${Math.floor(timer / 60) < 10 ? "0" : ""}${Math.floor(
     timer / 60
   )}:${timer % 60 < 10 ? "0" : ""}${timer % 60}`;
   return (
-    <RootContext.Provider value={{ timer: displayTimer, startTimer, pauseTimer }}>
+    <RootContext.Provider
+      value={{
+        active,
+        timer: displayTimer,
+        setActive,
+        startTimer,
+        pauseTimer,
+        clearTimer,
+      }}
+    >
       {children}
     </RootContext.Provider>
   );
