37457a042340cf1df7250c063f65463b1f8354cf
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 18:53:40 +0800

    updated: render the modal in the dom when component is mounted

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,28 +5,35 @@ import { HiChevronDown } from "react-icons/hi";
 export default function Modal() {
   const modalState = React.useRef(null);
   const { modal, setModal } = React.useContext(RootContext);
+
+  const handleClick = (event) => {
+    if (modalState.current && !modalState.current.contains(event.target)) {
+      setModal(false);
+    }
+  };
   return (
     <>
-      {modal ? (
-        <div className="modal-background">
-          <div className="modal">
-            <div className="modal__flex">
-              <div className="modal__item">
-                <h1 className="modal__item-title">Background</h1>
-                <div className="modal__item-input">
-                  <div className="modal__item-box">
-                    <span className="modal__item-text">Hey</span>
-                    <div className="modal__item-icon-container">
-                      <HiChevronDown className="modal__item-icon" />
-                    </div>
+      <div
+        className={`modal-background ${modal ? "modal-background-open" : ""}`}
+        onClick={handleClick}
+      >
+        <div className={`modal ${modal ? "modal-open" : ""}`} ref={modalState}>
+          <div className="modal__flex">
+            <div className="modal__item">
+              <h1 className="modal__item-title">Background</h1>
+              <div className="modal__item-input">
+                <div className="modal__item-box">
+                  <span className="modal__item-text">Hey</span>
+                  <div className="modal__item-icon-container">
+                    <HiChevronDown className="modal__item-icon" />
                   </div>
-                  <div className="modal__item-appear"></div>
                 </div>
+                <div className="modal__item-appear"></div>
               </div>
             </div>
           </div>
         </div>
-      ) : null}
+      </div>
     </>
   );
 }
