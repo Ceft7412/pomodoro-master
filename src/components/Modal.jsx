82d960b3a9d0231b66b2540a01653067f323ccb0
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 19:02:36 +0800

    created: function to handle box appear when clicked

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,14 +5,22 @@ import { HiChevronDown } from "react-icons/hi";
 export default function Modal() {
   const modalState = React.useRef(null);
   const { modal, setModal } = React.useContext(RootContext);
-
+  const [box, setBox] = React.useState();
   const handleClick = (event) => {
     if (modalState.current && !modalState.current.contains(event.target)) {
       setModal(false);
     }
   };
 
-    
+  const handleBoxAppear = (box) => {
+    if (box === 1) {
+      setBox(1);  
+    } else if (box === 2) {
+      setBox(2);
+    } else if (box === 3) {
+      setBox(3);
+    }
+  };
   return (
     <>
       <div
@@ -24,7 +32,7 @@ export default function Modal() {
             <div className="modal__item">
               <h1 className="modal__item-title">Background</h1>
               <div className="modal__item-input">
-                <div className="modal__item-box">
+                <div className="modal__item-box" onClick={() => handleBoxAppear(1)}>
                   <span className="modal__item-text">Hey</span>
                   <div className="modal__item-icon-container">
                     <HiChevronDown className="modal__item-icon" />
