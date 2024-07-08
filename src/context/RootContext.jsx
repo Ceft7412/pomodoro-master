305b92595936a5413f1b7cbfebe9b8db0d3379dd
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 18:08:40 +0800

    updated: conditionals to change the timer depending on the active state

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -4,8 +4,14 @@ import React from "react";
 const RootContext = React.createContext();
 
 const RootProvider = ({ children }) => {
-  const choseTimer = 1500;
-  const [timer, setTimer] = React.useState(choseTimer);
+  let pomodoroTimer = 1500;
+  let shortBreakTimer = 300;
+  let longBreakTimer = 900;
+
+  const [timer, setTimer] = React.useState(pomodoroTimer);
+  const [pause, setPause] = React.useState(false);
+  const [modal, setModal] = React.useState(false);
+
   const [active, setActive] = React.useState({
     pomodoro: true,
     shortbreak: false,
@@ -16,14 +22,25 @@ const RootProvider = ({ children }) => {
 
   const startTimer = () => {
     interval.current = setInterval(() => {
-      setTimer((prevTime) => prevTime - 1);
+      setTimer((prevTime) => {
+        if (prevTime <= 1) {
+          clearInterval(interval.current);
+          interval.current = null;
+          pauseTimer();
+          if (active.pomodoro) {
+            return pomodoroTimer;
+          }
+          if (active.shortbreak) {
+            return shortBreakTimer;
+          }
+          if (active.longbreak) {
+            return longBreakTimer;
+          }
+        } else {
+          return prevTime - 1;
+        }
+      });
     }, 1000);
-
-    if (timer === 0) {
-      clearInterval(interval.current);
-      interval.current = null;
-      setTimer(choseTimer);
-    }
   };
 
   const pauseTimer = () => {
@@ -36,13 +53,16 @@ const RootProvider = ({ children }) => {
   const clearTimer = () => {
     clearInterval(interval.current);
     interval.current = null;
-    setTimer(choseTimer);
+    if (active.pomodoro) setTimer(pomodoroTimer);
+    if (active.shortbreak) setTimer(shortBreakTimer);
+    if (active.longbreak) setTimer(longBreakTimer);
   };
   const displayTimer = `${Math.floor(timer / 60) < 10 ? "0" : ""}${Math.floor(
     timer / 60
   )}:${timer % 60 < 10 ? "0" : ""}${timer % 60}`;
 
   const handleClick = (newState) => {
+    setPause(false);
     setActive((prevActive) => {
       // Create a new object with all states set to false
       const newStateObject = Object.keys(prevActive).reduce((acc, key) => {
@@ -55,12 +75,36 @@ const RootProvider = ({ children }) => {
 
       return newStateObject;
     });
+    switch (newState) {
+      case "pomodoro":
+        setTimer(pomodoroTimer);
+        pauseTimer();
+        break;
+      case "shortbreak":
+        setTimer(shortBreakTimer);
+        pauseTimer();
+        break;
+      case "longbreak":
+        setTimer(longBreakTimer);
+        pauseTimer();
+        break;
+      default:
+        break;
+    }
   };
   return (
     <RootContext.Provider
       value={{
         active,
+        pause,
+        pomodoroTimer,
+        shortBreakTimer,
+        longBreakTimer,
+        modal,
         timer: displayTimer,
+        setModal,
+        setTimer,
+        setPause,
         setActive,
         startTimer,
         pauseTimer,
