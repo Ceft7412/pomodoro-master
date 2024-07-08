215152eb6420d9d67831004bf63b96f86405503c
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-08 18:30:43 +0800

    added: classes for the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { RootContext } from "@/context/RootContext";
+import { HiChevronDown } from "react-icons/hi";
+
 export default function Modal() {
   const { modal } = React.useContext(RootContext);
   return (
@@ -7,7 +9,20 @@ export default function Modal() {
       {modal ? (
         <div className="modal-background">
           <div className="modal">
-            <span>EUT</span>
+            <div className="modal__flex">
+              <div className="modal__item">
+                <h1 className="modal__item-title">Background</h1>
+                <div className="modal__item-input">
+                  <div className="modal__item-box">
+                    <span className="modal__item-text">Hey</span>
+                    <div className="modal__item-icon-container">
+                      <HiChevronDown className="modal__item-icon" />
+                    </div>
+                  </div>
+                  <div className="modal__item-appear"></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       ) : null}
