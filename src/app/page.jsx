54673f6b16fbf33c005ea0b2faa829c97cbbf54d
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-09 16:49:00 +0800

    added: ref for modalinfo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,8 @@ import { BsArrowRepeat } from "react-icons/bs";
 import { GrFormNextLink } from "react-icons/gr";
 import { VscSettings } from "react-icons/vsc";
 import { RootContext } from "@/context/RootContext";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 export default function Home() {
   const {
@@ -28,6 +30,21 @@ export default function Home() {
     shortBreakTimer,
     longBreakTimer,
   } = React.useContext(RootContext);
+  const modalInfoRef = React.useRef(null);
+  const [modalInfo, setModalInfo] = React.useState(false);
+  console.log(modalInfo);
+  React.useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      e.preventDefault();
+      e.returnValue = "Your changes will be lost if you reload the page. Are you sure?";
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
 
   const handleTimer = () => {
     if (pause === true) {
@@ -85,6 +102,12 @@ export default function Home() {
       return newStateObject;
     });
   };
+
+  const handleClickInfo = (event) => {
+    if (modalInfoRef.current && !modalInfoRef.current.contains(event.target)) {
+      setModalInfo(false);
+    }
+  };
   return (
     <>
       <div className="skeleton">
@@ -102,7 +125,49 @@ export default function Home() {
           <div className="background__settings-icon" onClick={() => setModal(!modal)}>
             <VscSettings fontSize={25} />
           </div>
-
+          <div className="background__info-icon" onClick={() => setModalInfo(!modalInfo)}>
+            <IoMdInformationCircleOutline fontSize={25} />
+          </div>
+          <div
+            className={`background__info-overlay ${
+              modalInfo ? "background__info-overlay-open" : ""
+            }`}
+            onClick={handleClickInfo}
+          >
+            <div
+              ref={modalInfoRef}
+              className={`background__info-modal ${modalInfo ? "info-open" : ""}`}
+            >
+              <div className="background__info-flex">
+                <div className="background__info-header">
+                  <span>PomodoroMaster by Ceft</span>
+                  <div
+                    className="background__infos-icon"
+                    onClick={() => setModalInfo(false)}
+                  >
+                    <IoClose fontSize={20} />
+                  </div>
+                </div>
+                <div className="background__info-body">
+                  <p>
+                    Feel free to reach out if you have any inquiries or would like to
+                    suggest additional features. Your feedback is always welcome!
+                  </p>
+                  <a
+                    href="https://www.linkedin.com/in/cedrick-caceres-22b8612b4/"
+                    className="background__info-icon-container"
+                  >
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg"
+                      width="40"
+                      height="40"
+                      alt="Cedrick Caceres"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
           <Menu />
           <div className="content">
             <div className="content__flex-col">
