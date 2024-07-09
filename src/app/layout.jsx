99c3169ef45957f464962b1af39fa1b4b7570786
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-09 16:48:52 +0800

    added: metadata of the site

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,10 @@ import "../resources/styles.scss";
 
 // const inter = Inter({ subsets: ["latin"] });
 import { RootProvider, RootContext } from "@/context/RootContext";
-
+export const metadata = {
+  description:
+    "Pomodoro master is an app that is easy to use and helps you to focus on your tasks. You can customize the app to your liking. Choose your aesthetic themes without hassle!",
+};
 export default function RootLayout({ children }) {
   return (
     <RootProvider>
