9359ca4cb8e5deb8a457849bc446d531c807f958
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-09 17:07:33 +0800

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,16 +7,15 @@ import "../resources/styles.scss";
 
 // const inter = Inter({ subsets: ["latin"] });
 import { RootProvider, RootContext } from "@/context/RootContext";
-
+const metadata = {
+  description:
+    "Pomodoro master is an app that is easy to use and helps you to focus on your tasks. You can customize the app to your liking. Choose your aesthetic themes without hassle!",
+};
 export default function RootLayout({ children }) {
   return (
     <RootProvider>
       <html lang="en">
         <head>
-          <meta
-            name="description"
-            content="Pomodoro master is an app that is easy to use and helps you to focus on your tasks. You can customize the app to your liking. Choose your aesthetic themes without hassle!"
-          />
           <Title />
         </head>
         <Body>{children}</Body>
